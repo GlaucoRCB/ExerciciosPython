c603f8bb334eb587af2ab2f5c6f9d2a88fce504d
unknown <glaucorodriguescb@gmail.com>
2025-11-06 12:42:57 -0300

    correção ex 22

--- Desafio22.py
+++ Desafio22.py
@@ -5,10 +5,9 @@ pais_capital = {
     "Japão": 'Tóquio',
     "Canadá": 'Ottwa'
 }
-
 pesquisa_pais = input('Digite o nome de um pais: ')
 
 if pesquisa_pais in pais_capital:
-    print(f'A capital do pais {pesquisa_pais} é {pais_capital['Brasil']}')
+    print(f'A capital do pais {pesquisa_pais} é {pais_capital[pesquisa_pais]}')
 else:
     print('Desculpe não temos informações sobre a capital desse país')
